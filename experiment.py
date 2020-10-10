39ef8f588bc037572868fa3733512ffe37461ad0
lee-man <minli.amoy@gmail.com>
2020-10-10 10:24:34 +0800

    update.

--- experiment.py
+++ experiment.py
@@ -45,7 +45,7 @@ class ClsExperiment(pl.LightningModule):
         val_loss = F.cross_entropy(outputs, targets)
         _, predicted = outputs.max(1)
         acc = accuracy(predicted, targets)
-        self.log('val_acc', acc.item(), on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log('val_acc', acc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         # self.log('val_loss', val_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return val_loss
     
